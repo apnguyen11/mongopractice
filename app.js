2e9fd64b03563e80b55a6e8b86db34305d6d4bf7
Andy Nguyen <apnguyen11@yahoo.com>
2019-10-31 18:05:48 -0500

    added post request to add new player to database. tested via postman

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Players = require('./player.js')
+var bodyParser = require('body-parser')
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost/mongoWorkshop')
 .then(()=> console.log('Mangoose connectd'))
 //CRUD
-app.get('/', (req, res) => {
+
+//get all players
+app.get('/players', (req, res) => {
    Players.find()
    .then(data=>res.json(data))
    .catch(err=>console.log(err))
 })
-app.post('/', (req, res) => {
-    res.send('posting')
+
+//create player
+app.post('/players/new', (req, res) => {
+    console.log(req.body)
+    Players.create(req.body)
+    .then(data => res.json(data))
+    .catch(err => console.log(err))
  })
  app.patch('/', (req, res) => {
     res.send('patching')
