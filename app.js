c87f6616553a3e6b5336b2829026d9538819f5ff
Andy Nguyen <apnguyen11@yahoo.com>
2019-10-31 19:54:38 -0500

    added patch request to update player information

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost/mongoWorkshop')
 .then(()=> console.log('Mangoose connectd'))
 //CRUD
-
+mongoose.set('useFindAndModify', false);
 //get all players
 app.get('/players', (req, res) => {
    Players.find()
@@ -28,11 +28,20 @@ app.post('/players/new', (req, res) => {
     .then(data => res.json(data))
     .catch(err => console.log(err))
  })
- app.patch('/', (req, res) => {
-    res.send('patching')
+ app.patch('/players/:id', (req, res) => {
+    let {id} = req.params
+    console.log('GETTING ONE', req.params)
+    Players.findOneAndUpdate({_id: id},
+        req.body)
+    .then(data => res.json(data))
+    .catch(err => res.json(err))
  })
  app.delete('/', (req, res) => {
-    res.send('deleting')
+    Players.remove(
+        {_id: req.params.id}
+    )
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.json(err))
  })
 
 
