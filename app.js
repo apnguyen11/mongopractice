21573cdd334791db4bd69393d3cf1e819481e020
Andy Nguyen <apnguyen11@yahoo.com>
2019-11-01 12:05:31 -0500

    rendered all players in database to template page

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Players = require('./player.js')
 var bodyParser = require('body-parser')
+const mustache = require('mustache')
 const app = express();
+const fs = require('fs');
 const port = 3000;
+mongoose.set('useFindAndModify', false);
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -12,13 +15,30 @@ app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost/mongoWorkshop')
 .then(()=> console.log('Mangoose connectd'))
+
+const teamTemplate = fs.readFileSync('./templates/team.mustache', 'utf8')
 //CRUD
-mongoose.set('useFindAndModify', false);
+function getAllPlayers(){
+    return Players.find()
+    // .then(data => res.json(data))
+    // .catch(err=> console.log(err))
+}
+
+function renderPlayer (player) {
+    return `<li>${player}</li>`
+  }
+
+function renderAllPlayers(allPlayers){
+    return '<ul>' + allPlayers.map(renderPlayer).join('') + '</ul>'
+}
+
+
 //get all players
-app.get('/players', (req, res) => {
-   Players.find()
-   .then(data=>res.json(data))
-   .catch(err=>console.log(err))
+app.get('/', (req, res) => {
+   getAllPlayers()
+    .then(function(allPlayers){
+        res.send(mustache.render(teamTemplate, {baseballteam: renderAllPlayers(allPlayers)}))
+    })
 })
 
 //create player
